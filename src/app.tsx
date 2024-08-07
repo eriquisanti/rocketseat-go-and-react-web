e19dec70f5d17a57d21603f32a416a71521973a8
Ériqui Santi <eriqui.santi@aggrandize.com.br>
2024-08-07 12:04:37 -0300

    rocketseat web

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { CreateRoom } from "./pages/create-room"
+import { Room } from "./pages/room"
+import { Toaster } from 'sonner'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateRoom />
+  },
+  {
+    path: "/rooms/:roomId",
+    element: <Room />
+  }
+])
+
+export function App() {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </>
+  )
+}
+
+
